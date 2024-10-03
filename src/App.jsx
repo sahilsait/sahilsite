2ef2f5a227c2821f193546d49a269d6f8096ad5a
Sahil Sait <sahilsaitn@gmail.com>
2024-10-03 00:45:21 -0700

    updated resume link

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         </div>
         <div className='buttons'>
           <Link className='btn' to='#projects' smooth>Projects</Link>
-          <a className='btn' href='https://drive.google.com/file/d/14MyX81vf32ODL9deOaODz_hlGfsWY0cy/view?usp=sharing' target="_blank" rel="noopener noreferrer">Resume</a>
+          <a className='btn' href='https://drive.google.com/file/d/1QpkD3d5IjwL8V-n4aX9u9sF5rSl1ulo6/view?usp=sharing' target="_blank" rel="noopener noreferrer">Resume</a>
         </div>
       </div>
       <div id='projects'>
